4f0cc35f4ae1114046be4311622d5af6c31c8373
Tatiana Grigorieva <tsg1204@gmail.com>
2020-03-30 12:54:14 -0400

    expo web build

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Deploy to Heroku Test!</Text>
+      <Text>Deploy to Heroku Test! Deploy to Heroku Test!</Text>
     </View>
   );
 }
