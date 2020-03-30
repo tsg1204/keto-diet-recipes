2c8727b86b0e3f4e3d4c2f9132674849ec842d57
Tatiana Grigorieva <tsg1204@gmail.com>
2020-03-30 13:46:32 -0400

    Updates

---
+++ precache-manifest.32d5c5987cbbec21ae75dfe13a69a6c5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/expo-gh-pages/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "d9b9c7f98fcb4b02e9a9",
+    "url": "/expo-gh-pages/static/js/app.db7d8431.chunk.js"
+  },
+  {
+    "revision": "058af695c7a9a6a58d80",
+    "url": "/expo-gh-pages/static/js/runtime~app.257e9003.js"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/expo-gh-pages/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/expo-gh-pages/serve.json"
+  },
+  {
+    "revision": "98393fbf9882b1fcf99e",
+    "url": "/expo-gh-pages/static/js/2.92e0cde9.chunk.js"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/expo-gh-pages/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "e67e89aed07cc5dc9e187e7de522d504",
+    "url": "/expo-gh-pages/register-service-worker.js"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/expo-gh-pages/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "a485a1bbab5e28df9e15dedb9d7627d5",
+    "url": "/expo-gh-pages/manifest.json"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/expo-gh-pages/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/expo-gh-pages/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/expo-gh-pages/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-gh-pages/expo-service-worker.js"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/expo-gh-pages/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "04dfe1b9706727f00ce4bc7eeb7ae9f0",
+    "url": "/expo-gh-pages/index.html"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/expo-gh-pages/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/expo-gh-pages/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/expo-gh-pages/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/expo-gh-pages/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/expo-gh-pages/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/expo-gh-pages/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/expo-gh-pages/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/expo-gh-pages/apple/icons/icon_180x180.png"
+  }
+];
